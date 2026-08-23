218288f0896283e8d254b160f6ff24e38402ef8a
moody <moody@users.noreply.github.com>
2026-08-22 20:36:54 -0400

    add test for the shallow copy change to CreatePayloadReducer

--- test/store/create-payload-reducer-tests.lua
+++ test/store/create-payload-reducer-tests.lua
@@ -23,6 +23,19 @@ do
   assert(reducer(0, { type = "SET_VALUE", payload = 1 }) == 1)
 end
 
+-- Test Wux:CreatePayloadReducer() - a table payload is copied, so mutating
+-- it after being returned doesn't affect the stored state.
+do
+  local reducer = Wux:CreatePayloadReducer("SET_VALUE", {})
+  local payload = { count = 1 }
+  local newState = reducer(nil, { type = "SET_VALUE", payload = payload })
+  assert(newState ~= payload)
+  assert(newState.count == 1)
+
+  payload.count = 999
+  assert(newState.count == 1)
+end
+
 -- Test Wux:CreatePayloadReducer() - defaultState is not used when an
 -- initialState already provides a value for this slice.
 do
